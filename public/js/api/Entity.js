6ae8787a1375dd615ecc630dcc4e6c5e3b3363df
BlueberryDari <avlertaver@gmail.com>
2026-08-30 14:32:20 +0300

    Entity is ready

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,37 +1,34 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
+
 class Entity {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = () => {
-    if (this.xhr.status = 200) {
-      const data = JSON.stringify(this.xhr.responseText);
-    }
-  }
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list(data, callback){
 
+  static URL = ''; //статическое свойство у всего класса, для всех экземпляров
+  // статические методы работают внутри класса, не у экземпляра
+
+  static list(data, callback) {
+    createRequest({
+      url: this.URL,
+      data: data,
+      method: 'GET',
+      callback
+    });
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
+
   static create(data, callback) {
-
+    createRequest({
+      url: this.URL,
+      data: data,
+      method: 'PUT',
+      callback
+    });
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static remove(data, callback ) {
-
+  static remove(data, callback) {
+    createRequest({
+      url: this.URL,
+      data: data,
+      method: 'DELETE',
+      callback
+    });
   }
 }
