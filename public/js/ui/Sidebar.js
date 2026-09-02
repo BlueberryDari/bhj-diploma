352d5275c95a41f6f6aa772b51d97df936b82ac6
BlueberryDari <avlertaver@gmail.com>
2026-09-02 18:05:18 +0300

    Sidebar is ready

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,49 +1,64 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
 
-const sidebarBurger = document.querySelector('.sidebar-toggle');
-const body = document.getElementsByTagName('body')[0];
 
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
+
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
+
+    const sidebarBurger = document.querySelector('.sidebar-toggle');
+    const body = document.body;
+
 
     if (!sidebarBurger || !body) return;
 
-    sidebarBurger.addEventListener('click',(e) => {
+    sidebarBurger.addEventListener('click', (e) => {
 
       e.preventDefault(); //Отменяем стандартное поведение ссылки
 
       body.classList.toggle('sidebar-open');
       body.classList.toggle('sidebar-collapse');
-    });  
+    });
   }
 
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
   static initAuthLinks() {
-menu-item_login
-menu-item_register
-menu-item_logout
-  }
+
+    const menu = document.querySelector('.sidebar');
+    if (!menu) return;
+
+    const loginBtn = menu.querySelector('.menu-item_login');
+    loginBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const loginModal = document.getElementById('modal-login');
+      const modalWindow = App.getModal(loginModal);
+
+      Modal.open(modalWindow);
+    })
+
+    const registerBtn = menu.getElementsByClassName('menu-item_register')[0];
+    registerBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const registerModal = document.getElementById('modal-register');
+      const modalWindow = App.getModal(registerModal);
+
+      Modal.open(modalWindow);
+    })
+
+    const logoutBtn = menu.getElementsByClassName('menu-item_logout')[0];
+    logoutBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      User.logout((response => {
+        if (response && response.success) {
+          App.setState('init');
+        }
+      })
+      )
+    });
+  };
 }
