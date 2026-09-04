162ece5d914d86c5e10d31e459ea5701ff8413a0
BlueberryDari <avlertaver@gmail.com>
2026-09-04 13:57:07 +0300

    RegisterForm is edited, check for the Element is added

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,15 +1,20 @@
 //const { response } = require("express");
 
 class RegisterForm extends AsyncForm {
- 
+
   onSubmit(data) {
     User.register(data, (response) => {
       if (response && response.success) {
         this.element.reset();
         App.setState('user-logged');
 
-        const neededWindow = App.getModal(this.element.closest('.modal'));
-        Modal.close(neededWindow);
+        const modalEl = this.element.closest('.modal');
+        if (modalEl) {
+          const neededWindow = App.getModal(modalEl);
+          if (neededWindow) {
+            Modal.close(neededWindow);
+          }
+        }
       }
     });
   }
